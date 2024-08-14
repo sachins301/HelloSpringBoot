a8a9ad113513ba85a53fb49c23b68fcb6282efbb
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-08-13 21:10:41 -0600

    Metadata checkpoint

---
+++ src/main/java/com/springtutorial/photoz/photoz_clone/Photo.java
@@ -0,0 +1,34 @@
+package com.springtutorial.photoz.photoz_clone;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class Photo {
+    private String id;
+    @NotEmpty
+    private String fileName;
+
+    public Photo(){
+        //ToDo
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
---
+++ src/test/java/com/springtutorial/photoz/photoz_clone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorial.photoz.photoz_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
